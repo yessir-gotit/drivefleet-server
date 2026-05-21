0568b3a2f33d3adcb557ef6405c7f5f4033c211d
yessir-gotit <218499181+yessir-gotit@users.noreply.github.com>
2026-05-21 20:14:23 +0600

    Idk what to say anymore

--- auth.js
+++ auth.js
@@ -9,32 +9,37 @@ const db = client.db("drivefleetDB");
 
 const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
-  database: mongodbAdapter(db, {
-    client: client,
-  }),
+  database: mongodbAdapter(db, { client }),
 
   trustedOrigins: [
     "http://localhost:3000",
     process.env.FRONTEND_PRODUCTION_URL,
-  ],
+  ].filter(Boolean),
+
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
   },
+
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
   },
+
   account: {
     storeStateStrategy: "database",
-    skipStateCookieCheck: true,
   },
+
   advanced: {
+    crossSubdomainCookies: {
+      enabled: false,
+    },
     defaultCookieAttributes: {
       sameSite: "none",
       secure: true,
+      httpOnly: true,
     },
   },
 });
