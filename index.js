47f815a8bb6b5b30dd01357368147c0c4b57e626
yessir-gotit <218499181+yessir-gotit@users.noreply.github.com>
2026-05-21 16:56:05 +0600

    optimized for render

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      process.env.FRONTEND_PRODUCTION_URL,
+    ].filter(Boolean),
     credentials: true,
   }),
 );
