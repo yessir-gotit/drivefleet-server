70fff9410e1b97390db263e400588cfb3c28af65
yessir-gotit <218499181+yessir-gotit@users.noreply.github.com>
2026-05-21 17:49:49 +0600

    last try trust me (2) T_T

--- auth.js
+++ auth.js
@@ -8,6 +8,7 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("drivefleetDB");
 
 const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: mongodbAdapter(db, {
     client: client,
   }),
